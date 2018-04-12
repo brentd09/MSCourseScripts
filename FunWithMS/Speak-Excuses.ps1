5f47562b6f1082dae2567962fcb62ea0dc21c26a
brent denny <brentdenny@gmail.com>
2018-04-13 00:43:15 +1000

    Added a bit of code to the speak-excuse

--- FunWithMS/Speak-Excuses.ps1
+++ FunWithMS/Speak-Excuses.ps1
@@ -3,8 +3,11 @@
 
 [Reflection.Assembly]::LoadWithPartialName('System.Speech') | Out-Null 
 $webRaw = Invoke-WebRequest -uri "http://pages.cs.wisc.edu/~ballard/bofh/excuses"
-$Excuses =  $webRaw.RawContent -split "`n" | Select-Object -skip 9 | Where-Object {$_ -notmatch "\/\\\'" }
-$phrase = Get-Random $Excuses
+$Excuses =  $webRaw.RawContent -split "`n" | Select-Object -skip 9 | Where-Object {$_ -notmatch "\/\\\'ex" }
+$Excuse1 = Get-Random $Excuses
+$Excuse2 = Get-Random $Excuses | where {$_ -ne $Excuse1}
 $object = New-Object System.Speech.Synthesis.SpeechSynthesizer 
-$object.SelectVoiceByHints('Female')
+$object.SelectVoiceByHints('Male')
+$phrase = "The problem you have with your computer was because of, " + $Excuse1 + " and " + $Excuse2
 $object.Speak($phrase)
+$object.Dispose()
