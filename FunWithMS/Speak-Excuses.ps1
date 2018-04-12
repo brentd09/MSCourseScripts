41b4425cba88250e18bd45c20b9b6736e0afcc02
Brent Denny <brentdenny@gmail.com>
2018-04-12 15:14:24 +1000

    Added fun stuff to do with speaker in windows

---
+++ FunWithMS/Speak-Excuses.ps1
@@ -0,0 +1,10 @@
+﻿# This will pull the BOFH excuse list and random an excuse and 
+# speak it through the speaker
+
+[Reflection.Assembly]::LoadWithPartialName('System.Speech') | Out-Null 
+$webRaw = Invoke-WebRequest -uri "http://pages.cs.wisc.edu/~ballard/bofh/excuses"
+$Excuses =  $webRaw.RawContent -split "`n" | Select-Object -skip 9 | Where-Object {$_ -notmatch "\/\\\'" }
+$phrase = Get-Random $Excuses
+$object = New-Object System.Speech.Synthesis.SpeechSynthesizer 
+$object.SelectVoiceByHints('Female')
+$object.Speak($phrase)
