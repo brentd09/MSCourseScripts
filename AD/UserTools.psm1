10e4eb74a9fb2b517d13cbe3b89015dd40e5d4f4
brent denny <brentdenny@gmail.com>
2018-05-01 10:50:02 +1000

    get-currentusersid added as a users tools module

---
+++ AD/UserTools.psm1
@@ -0,0 +1,13 @@
+﻿function Get-CurrentUserSID {
+[Cmdletbinding()]
+Param()
+$Template = @'
+User Name      SID
+============== ==============================================
+{UserName*:Domain1\usera} {SID:S-1-5-21-1955989083-2427161618-3948596988-1000}
+{UserName*:domainb\username1} {SID:S-1-5-21-1955989083-2427161618-3948596988-1001}
+{UserName*:flintstone\fred} {SID:S-1-5-21-1955989083-2427161618-3948596988-1002}
+'@
+
+whoami /user | ConvertFrom-String -TemplateContent $Template
+}
