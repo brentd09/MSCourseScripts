62f9b9009e2b3901fe71005c360a6a4d8f5eebaa
Brent Denny <brent.denny@ddls.com.au>
2018-05-30 09:29:51 +1000

    Added some finesse to the FSMO function

--- AD/ADMgmtTools.psm1
+++ AD/ADMgmtTools.psm1
@@ -1,19 +1,41 @@
 function Get-ADFsmoMasters {
+  <#
+  .Synopsis
+    Retrieves the FSMO Role holders
+  .DESCRIPTION
+    This command get the FSMO role owners from AD relative to the domain
+    controller the script is directed to. If no ADDC is chosen the default
+    if the current computer the script is run on.
+  .EXAMPLE
+    Get-ADFsmoMasters
+  .EXAMPLE
+    Get-ADFsmoMasters -DomainController dc1.company.com
+  .PARAMETER DomainController
+    Points the script to a domain controller
+  .NOTES
+    General notes
+    Created By  : Brent Denny
+    Date Created: 30-May-2018
+  #>
   Param (
     [string]$DomainController = [system.net.dns]::GetHostByName($env:COMPUTERNAME)
   )
   # Query with the current credentials
-    $ForestInfo = Get-ADForest -Server $DomainController
-    $DomainInfo = Get-ADDomain -Server $DomainController
-  
+  try {
+    $ForestInfo = Get-ADForest -Server $DomainController -ErrorAction Stop
+    $DomainInfo = Get-ADDomain -Server $DomainController -ErrorAction Stop
+  }
+  catch {
+    Write-Warning "The FSMO Role owners were not able to be determined"
+    break
+  }
   # Define Properties
   $NewObjProp = [ordered]@{
-    SchemaMaster = $ForestInfo.SchemaMaster
-    DomainNamingMaster = $ForestInfo.DomainNamingMaster
+    SchemaMaster         = $ForestInfo.SchemaMaster
+    DomainNamingMaster   = $ForestInfo.DomainNamingMaster
     InfraStructureMaster = $DomainInfo.InfraStructureMaster
-    RIDMaster = $DomainInfo.RIDMaster
-    PDCEmulator = $DomainInfo.PDCEmulator
+    RIDMaster            = $DomainInfo.RIDMaster
+    PDCEmulator          = $DomainInfo.PDCEmulator
   }
-  
   New-Object -TypeName PSObject -Property $NewObjProp
 }
