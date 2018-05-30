03a587a5135d790882174918af8091643b0c40da
Brent Denny <brent.denny@ddls.com.au>
2018-05-30 10:15:51 +1000

    FSMO added a lot of error checking

--- AD/ADMgmtTools.psm1
+++ AD/ADMgmtTools.psm1
@@ -5,25 +5,47 @@ function Get-ADFsmoMasters {
   .DESCRIPTION
     This command get the FSMO role owners from AD relative to the domain
     controller the script is directed to. If no ADDC is chosen the default
-    if the current computer the script is run on.
+    if the current computer the script is run on. This also accepts the 
+    NETBIOS name of the domain controller and will translate it into 
+    the FQDN. 
   .EXAMPLE
     Get-ADFsmoMasters
   .EXAMPLE
     Get-ADFsmoMasters -DomainController dc1.company.com
   .PARAMETER DomainController
-    Points the script to a domain controller
+    Points the script to a domain controller this could either be the FQDN 
+    or the NETBIOS name both will work as long as DNS can resolve this name 
+    to the FQDN.
   .NOTES
     General notes
     Created By  : Brent Denny
     Date Created: 30-May-2018
   #>
   Param (
-    [string]$DomainController = [system.net.dns]::GetHostByName($env:COMPUTERNAME)
+    [string]$DomainController = ([system.net.dns]::GetHostByName($env:COMPUTERNAME)).HostName
   )
   # Query with the current credentials
   try {
-    $ForestInfo = Get-ADForest -Server $DomainController -ErrorAction Stop
-    $DomainInfo = Get-ADDomain -Server $DomainController -ErrorAction Stop
+    $DomainController = ([system.net.dns]::GetHostByName($DomainController)).HostName
+    $ErrorActionPreference = "Stop"
+    $Ldap = '_ldap._tcp.' + $env:USERDNSDOMAIN
+    $DnsAnswer = Resolve-DnsName -Type SRV $Ldap
+    $DCs = $DnsAnswer.NameTarget
+  }
+  Catch {
+    Write-Warning "There appears to be a problem, wrong name or DNS resolving issue"
+    break
+  }
+  Finally {
+    $ErrorActionPreference = "Continue"
+  }
+  if ($DomainController -notin $DCs) {
+    Write-Warning "$DomainController is not a Domain Controller"
+    break
+  }
+  try {
+    $ForestInfo = Invoke-Command -ComputerName $DomainController -ScriptBlock {Get-ADForest} -ErrorAction Stop
+    $DomainInfo = Invoke-Command -ComputerName $DomainController -ScriptBlock {Get-ADDomain} -ErrorAction Stop
   }
   catch {
     Write-Warning "The FSMO Role owners were not able to be determined"
