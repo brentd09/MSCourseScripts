bf4036da10097f1b0a1aa821f59f331a143564f8
BrentD09 <brent.denny@ddls.com.au>
2018-07-31 05:22:05 +0000

    Using PS commands to achieve a better result

--- AD/ADMetadataInfo.ps1
+++ AD/ADMetadataInfo.ps1
@@ -1,17 +1,14 @@
-﻿$data = @'
-Loc.USN                           Originating DSA  Org.USN  Org.Time/Date        Ver Attribute
-=======                           =============== ========= =============        === =========
-   {[int]LocalUSN*:8201} {OrigDSA:9dea5320-9c83-4838-bbb6-fca0ca93f752}  {[int]OrigUSN:8196} {[datetime]OrigTime:2016-10-18 12:47:42} {[int]Version:1} {[string]Attribute:objectClass}
-   {[int]LocalUSN*:8201} {OrigDSA:Default-First-Site-Name\TOR-DC1}       {[int]OrigUSN:8201} {[datetime]OrigTime:2018-04-09 23:11:54} {[int]Version:1} {[string]Attribute:cn}
-   {[int]LocalUSN*:8201} {OrigDSA:9dea5320-9c83-4838-bbb6-fca0ca93f752}  {[int]OrigUSN:8196} {[datetime]OrigTime:2016-10-18 12:47:42} {[int]Version:1} {[string]Attribute:description}
-   {[int]LocalUSN*:8201} {OrigDSA:9dea5320-9c83-4838-bbb6-fca0ca93f752}  {[int]OrigUSN:8196} {[datetime]OrigTime:2016-10-18 12:47:42} {[int]Version:2} {[string]Attribute:descriptionA}
-   {[int]LocalUSN*:1234} {OrigDSA:9dea5320-9c83-4838-bbb6-fca0ca93f752}  {[int]OrigUSN:345}  {[datetime]OrigTime:2016-10-18 12:47:42} {[int]Version:3} {[string]Attribute:descriptionB}
-0 entries.
-Type    Attribute     Last Mod Time                            Originating DSA  Loc.USN Org.USN Ver
-======= ============  =============                           ================= ======= ======= ===
-        Distinguished Name
-        =============================
-'@
-
-$RepAdminRaw = repadmin /showobjmeta tor-dc1 "cn=administrator,cn=users,dc=adatum,dc=com"
-$RepAdminRaw |  ConvertFrom-String -TemplateContent $data -ErrorAction SilentlyContinue 
+ function Get-ADObjectMeta {
+  Param (
+    $UserName = 'Administrator'
+  )
+  Get-ADUser -Filter {Name -eq $UserName} | 
+    Get-ADReplicationAttributeMetadata -server lon-dc1 | 
+      Select-Object  @{n='Attribute';e={$_.AttributeName}},
+                     @{n='Server';e={$_.Server}},
+                     @{n='LocalUSN';e={$_.LocalchangeUSN}},
+                     @{n='OrigServer';e={$_.LastOriginatingChangeDirectoryServerInvocationId}},
+                     @{n='OrigUSN';e={$_.LastOriginatingChangeUSN}},
+                     @{n='ChangeTime';e={$_.LastOriginatingChangeTime}},
+                     @{n='Version';e={$_.Version}}
+} 
