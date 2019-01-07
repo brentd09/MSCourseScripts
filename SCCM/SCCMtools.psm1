6057c427ef0cba7974e0b14815c331b0157ff842
Brent Denny <brent.denny@ddls.com.au>
2019-01-07 17:08:49 +1100

    Write the error checking for the SCCM module

--- SCCM/SCCMtools.psm1
+++ SCCM/SCCMtools.psm1
@@ -1,19 +1,19 @@
 ﻿function Add-CMNamesToCollection {
   <#
   .SYNOPSIS
-     Add Computers into a Device Collection 
+     Add Computers into a Device Collection
   .DESCRIPTION
      This adds a list of computers into a Device Collection from a text file
-     the text file must contain only the names of each computer and each 
+     the text file must contain only the names of each computer and each
      computername must be on a seperate line. If the Collection exists it just
-     adds the computers to that collection if not it will create the collection 
+     adds the computers to that collection if not it will create the collection
      first.
   .EXAMPLE
      Add-CMNamesToCollection -SiteCode S01 -CollectionName Coll001 -FileName c:\computers.txt
   .EXAMPLE
      Add-CMNamesToCollection -SiteCode S01 -CollectionName Coll001 -FileName c:\computers.txt -LimitingCollection "All Systems"
   .NOTES
-     General notes
+     General Notes
      Created by: Brent Denny
      Created on: 7 Jan 2019
   #>
@@ -25,21 +25,36 @@
     [string]$CollectionName,
     [Parameter(Mandatory=$true)]
     [string]$Filename,
-    [string]$LimitingCollection = "All Systems"  
+    [string]$LimitingCollection = "All Systems"
   )
-  
-  $SiteCode = ($SiteCode + ':') -replace "\:[2,]",':'
-  Set-Location $SiteCode
-  $CurrentCollections =  Get-CMCollection 
-  if ($CurrentCollections.name -notcontains $CollectionName) {
-    $Collection = New-CMDeviceCollection -Name $CollectionName -LimitingCollectionName $LimitingCollection
+  $SiteCode = $SiteCode -replace "(\w{3}).*",'$1'
+  $SiteCodeFull = $SiteCode + ':'
+  $ModulesLoaded = Get-Module
+  $PSDrives = Get-PSDrive
+  if ($ModulesLoaded.Name -contains 'ConfigurationManager' -and $PSDrives -contains $SiteCode) {
+    Set-Location $SiteCodeFull
+    $CurrentCollections =  Get-CMCollection
+    if ($CurrentCollections.name -notcontains $CollectionName) {
+      $Collection = New-CMDeviceCollection -Name $CollectionName -LimitingCollectionName $LimitingCollection
+    }
+    else {
+      $Collection = Get-CMCollection | Where-Object {$_.Name -eq $CollectionName}
+    }
+    $PCs = Get-Content $Filename | where-Object {$_ -match '\w'}
+    $DevicesToAdd = Get-CMResource -Fast | Where-Object {$_.Name -in $Pcs}
+    if ($DevicesToAdd.Count -ne 0) {
+      foreach ($Device in $DevicesToAdd) {
+        Add-CMDeviceCollectionDirectMembershipRule -CollectionId $Collection.CollectionID -ResourceID $Device.ResourceID 
+      }
+    }
+    else {
+      Write-Warning 'There were no matching computer objects found to add'
+    }
+    if ($DevicesToAdd.Count -ne $PCs.Count) {
+      Write-Warning 'Not all computers were found in Confiruation Manager'
+    }
   }
   else {
-    $Collection = Get-CMCollection | Where-Object {$_.Name -eq $CollectionName}
-  }
-  $PCs = Get-Content $Filename
-  $DevicesToAdd = Get-CMResource -Fast | where {$_.Name -in $Pcs} 
-  foreach ($Device in $DevicesToAdd) {
-    Add-CMDeviceCollectionDirectMembershipRule -CollectionId $Collection.CollectionID -ResourceID $Device.ResourceID 
+    Write-Warning "This needs to be run from a Configuation Manager Powershell window!"
   }
 }
