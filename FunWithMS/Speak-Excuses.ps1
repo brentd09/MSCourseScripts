2dbab4e52fbb91f1b06d4b22c698792845972ced
Brent Denny <brent.denny@ddls.com.au>
2019-02-07 13:52:49 +1100

    modified the speak excuses script

--- FunWithMS/Speak-Excuses.ps1
+++ FunWithMS/Speak-Excuses.ps1
@@ -1,13 +1,17 @@
-﻿# This will pull the BOFH excuse list and random an excuse and 
+﻿[CmdletBinding()]
+Param (
+  [string]$Excuse = "none"
+)
+# This will pull the BOFH excuse list and random an excuse and 
 # speak it through the speaker
 
 [Reflection.Assembly]::LoadWithPartialName('System.Speech') | Out-Null 
 $webRaw = Invoke-WebRequest -uri "http://pages.cs.wisc.edu/~ballard/bofh/excuses"
 $Excuses =  $webRaw.RawContent -split "`n" | Select-Object -skip 9 | Where-Object {$_ -notmatch "\/\\\'ex" }
-$Excuse1 = Get-Random $Excuses
-$Excuse2 = Get-Random $Excuses | where {$_ -ne $Excuse1}
+if ($Excuse -eq 'none') {$Excuse1 = Get-Random $Excuses}
+else {$Excuse1 = $Excuse}
 $object = New-Object System.Speech.Synthesis.SpeechSynthesizer 
 $object.SelectVoiceByHints('Male')
-$phrase = "The problem you have with your computer was because of, " + $Excuse1 + " and " + $Excuse2
+$phrase = "The problem you have with your computer was because of, " + $Excuse1 
 $object.Speak($phrase)
 $object.Dispose()
