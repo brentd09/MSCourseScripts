b2a42bdbb4c794c34b34519621c8884f8bffa457
Brent Denny <brent.denny@ddls.com.au>
2019-03-26 10:34:20 +1000

    Setting up course scripts for MS 2016 courses

---
+++ CourseScripts/MS20740/Remove-SMBv1DomainWide.ps1
@@ -0,0 +1,40 @@
+<#
+.SYNOPSIS
+  Gets or reports on the SMBv1 availability within your domain
+.DESCRIPTION
+  Long description
+.EXAMPLE
+  Remove-SMBv1DomainWide
+  This finds all computers in the domain and removes the SMBv1 dialect 
+  from each of them..
+.EXAMPLE
+  Remove-SMBv1DomainWide -ReportOnly
+  This finds all computers in the domain and reports on their SMBv1 
+  dialect status
+.NOTES
+  General notes
+  Created by: Brent Denny
+  Created on: 26 Mar 2019
+#>
+[CmdletBinding()]
+Param (
+  [switch]$ReportOnly
+)
+try {
+  $Computers = Get-AdComputer -filter * -ErrorAction Stop
+} 
+Catch {
+  Write-Warning "This script needs to be run from a machine that has the ActiveDirectory Module installed"
+}
+foreach ($Computer in $Computers) {
+  try {
+    Invoke-Command -ComputerName $Computer.Name -ScriptBlock -ErrorAction Stop {
+      $SMBServerConfig = Get-SmbServerConfiguration
+      if ($SMBServerConfig.EnableSMB1Protocol -eq $true -and $ReportOnly -eq $false) {
+        Set-SmbServerConfiguration -EnableSMB1Protocol $false
+      }
+      $SMBServerConfig | Select-Object -Property @{n='ComputerName';e={$_.PSComputerName}},EnableSMB1Protocol
+    }
+  }
+  catch {}
+}
