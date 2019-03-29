4150f033e400a96641e1ae86e9c3bf23d04496f7
Brent Denny <brent.denny@ddls.com.au>
2019-03-29 10:43:55 +1000

    RegEditbackrestore

---
+++ CourseScripts/MS20740/RegEditsBackupRestore.ps1
@@ -0,0 +1,14 @@
+﻿# Go to the user Registry
+Set-Location hkcu:
+
+# Create new registry key and give values 
+New-ItemProperty -Path .\bob -Name sister  -Value "jill" -PropertyType String
+Set-ItemProperty -Path .\bob -Name sister  -Value "Betty"
+
+# Backup the Reg Key and modify values
+Copy-Item -Path .\bob -Destination .\backupbob -Recurse
+Set-ItemProperty -Path .\bob -Name sister  -Value "Bobo"
+
+# Restore the RegKey, first need to remove the destination
+Remove-Item -Path .\bob -Recurse -Confirm:$false
+Copy-Item -Path .\backupbob -Destination .\bob -Recurse 
