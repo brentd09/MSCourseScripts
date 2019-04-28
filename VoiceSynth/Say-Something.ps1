b1883c6480dc62bca29692ab6911eb0720a13de9
Brent Denny <brent.denny@ddls.com.au>
2019-04-29 08:45:32 +1000

    Changes to voice scripts

---
+++ VoiceSynth/Say-Something.ps1
@@ -0,0 +1,13 @@
+﻿[CmdletBinding()]
+Param (
+  [Parameter(Mandatory=$true)]
+  [string]$WhatToSay
+)
+# This will pull the BOFH excuse list and random an excuse and 
+# speak it through the speaker
+
+[Reflection.Assembly]::LoadWithPartialName('System.Speech') | Out-Null 
+$object = New-Object System.Speech.Synthesis.SpeechSynthesizer 
+$object.SelectVoiceByHints('Male')
+$object.Speak($WhatToSay)
+$object.Dispose()
