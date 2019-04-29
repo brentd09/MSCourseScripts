d8d38fc322731fed78d6adc03a9b171ed088a57d
Brent Denny <brent.denny@ddls.com.au>
2019-04-29 22:09:18 +1000

    say something cleaned up the code

--- VoiceSynth/Say-Something.ps1
+++ VoiceSynth/Say-Something.ps1
@@ -3,8 +3,7 @@ Param (
   [Parameter(Mandatory=$true)]
   [string]$WhatToSay
 )
-# This will pull the BOFH excuse list and random an excuse and 
-# speak it through the speaker
+
 
 [Reflection.Assembly]::LoadWithPartialName('System.Speech') | Out-Null 
 $object = New-Object System.Speech.Synthesis.SpeechSynthesizer 
