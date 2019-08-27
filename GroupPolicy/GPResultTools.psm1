bdaca06d6518604170ab0d792fb2144140ce992d
Brent Denny <brent.denny@ddls.com.au>
2019-08-27 10:22:12 +1000

    fix help in gpo

--- GroupPolicy/GPResultTools.psm1
+++ GroupPolicy/GPResultTools.psm1
@@ -8,8 +8,30 @@
     Get-GPOProcess -ComputerName LON-CL1,LON-CL2,LON-CL3
     This command gets the RSOP information from GPResult and stores it in 
     XML files in the $Env:TEMP directory and then extracts the policy info
-    from these XML result files to produce an object that shows the policies
-    that were applied
+    from these XML result files to produce an object that shows the full 
+    policy process that was applied to the computer and user.
+    The results look like this:
+    ---------------------------
+    Starting user logon Policy processing for INSTRUCTOR\Administrator. 
+    Activity id: {435F6B9B-F242-4321-A80F-73AB713FB7B6}
+    The Group Policy processing mode is Foreground synchronous.
+    The loopback policy processing mode is "No loopback mode".
+    Group Policy receiving applicable GPOs from the domain controller.
+    Successfully completed downloading policies.
+    Group Policy successfully got applicable GPOs from the domain controller.
+    List of applicable Group Policy objects: 
+    
+    None
+    The following Group Policy objects were not applicable because they were filtered out :
+    
+    Local Group Policy
+    	Not Applied (Empty)
+    
+    Checking for Group Policy client extensions that are not part of the system.
+    Service configuration update to standalone is not required and will be skipped.
+    Finished checking for non-system extensions.
+    Completed user logon policy processing for INSTRUCTOR\Administrator in 0 seconds.
+    Group policy session completed successfully.
   .PARAMETER ComputerName
     One or more computernames can be added to compare the policys applied for each  
   .NOTES
