7319062ed6f9c58150240d1d5fc07d9d86801788
Brent Denny <brent.denny@ddls.com.au>
2019-12-12 22:05:08 +1000

    service tree mods still needs work

--- Services/Get-ServiceTree.ps1
+++ Services/Get-ServiceTree.ps1
@@ -1,28 +1,28 @@
-﻿Param ([string]$ServiceName = 'XblAuthManager')
-if ($Services.name -contains $ServiceName) {
-  Find-ServicesThatAreDependent -ServiceName $ServiceName -Index 0
-}
-else {Write-Warning "Service $($ServiceName) does not exist"}
+﻿[cmdletbinding()]
+Param ([string]$ServiceName = 'SamSS')
 
-
-function Find-ServicesThatAreDependent {
+function Find-ServiceTree {
   Param (
-    [string]$ServiceName,
+    [System.ServiceProcess.ServiceController]$Service,
     [int]$Index
   )    
-  $AllServicesWithDependencies = Get-Service | Where-Object  {$_.DependentServices.Count -gt 0}
-  foreach ($ServiceThatIsDependent in $AllServicesWithDependencies) {
-    if ($ServiceThatIsDependent.dependentservices.name -contains $ServiceName) {
-#      Write-Host -ForegroundColor Yellow $ServiceName
-      $Hash = @{ServiceName = $ServiceName; DependentService = $ServiceThatIsDependent.name;Index = $Index}
-      $Obj1 = New-Object -TypeName psobject -Property $Hash
-      $Obj1
-      Find-ServicesThatAreDependent -ServiceName $ServiceThatIsDependent.name -Level $Index
-      $SameServiceName = $ServiceName
-      $Index = $Index + 1
-    } 
+  $DependentServices = $Service.DependentServices
+  foreach ($DependentService in $DependentServices) {
+    Find-ServiceTree -Service $DependentService -Index 0
+    $ObjHash = [ordered]@{
+      Service = $Service.Name
+      DependentService = $DependentService.Name
+    }
+    New-Object -TypeName psobject -Property $ObjHash
   }
 }
+
+$AllServices = Get-Service 
+if ($AllServices.name -contains $ServiceName) {
+  $ServiceObj = Get-Service -Name $ServiceName
+  Find-ServiceTree -Service $ServiceObj -Index 0
+}
+else {Write-Warning "Service $($ServiceName) does not exist"}
 
 
 
