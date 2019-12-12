4a106ea89198e59539be717384165bbfcd8749e0
Brent Denny <brent.denny@ddls.com.au>
2019-12-12 15:42:57 +1000

    Added Service Tree script

---
+++ Services/Get-ServiceTree.ps1
@@ -0,0 +1,28 @@
+﻿Param ([string]$ServiceName = 'XblAuthManager')
+if ($Services.name -contains $ServiceName) {
+  Find-ServicesThatAreDependent -ServiceName $ServiceName -Index 0
+}
+else {Write-Warning "Service $($ServiceName) does not exist"}
+
+
+function Find-ServicesThatAreDependent {
+  Param (
+    [string]$ServiceName,
+    [int]$Index
+  )    
+  $AllServicesWithDependencies = Get-Service | Where-Object  {$_.DependentServices.Count -gt 0}
+  foreach ($ServiceThatIsDependent in $AllServicesWithDependencies) {
+    if ($ServiceThatIsDependent.dependentservices.name -contains $ServiceName) {
+#      Write-Host -ForegroundColor Yellow $ServiceName
+      $Hash = @{ServiceName = $ServiceName; DependentService = $ServiceThatIsDependent.name;Index = $Index}
+      $Obj1 = New-Object -TypeName psobject -Property $Hash
+      $Obj1
+      Find-ServicesThatAreDependent -ServiceName $ServiceThatIsDependent.name -Level $Index
+      $SameServiceName = $ServiceName
+      $Index = $Index + 1
+    } 
+  }
+}
+
+
+
