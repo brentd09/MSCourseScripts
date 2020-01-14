f421e3a187057fb5ffb7118ae2688a9d2e87aa70
Brent Denny <brent.denny@ddls.com.au>
2020-01-14 16:50:31 +1000

    Reg tools

---
+++ Registry/RegistryTools.psm1
@@ -0,0 +1,46 @@
+function Test-RegistryPath {
+  <#
+  .SYNOPSIS
+    This command tests to see if a Registry key or leaf/property exist
+  .DESCRIPTION
+    The command was created because of the limitations of the Test-Path command.
+    The Test-Path is limited and will only test the registry keys, it cannot help if
+    you are looking to test the existence of leaf/properties. The Test-RegistryPath 
+    command was the result of a question regarding how to test for both keys and 
+    leaf/properties in the registry.
+  .EXAMPLE
+    Test-RegistryPath -Path HKCU:\Software
+    This will test the existence of the HKCU:\Software key.
+  .EXAMPLE
+    Test-RegistryPath -Path HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\FirstLogon -Leaf
+    This will test the existence of the HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\FirstLogon 
+    leaf/property, the -Leaf parameter informs the test that it needs to check for Registry leaf/properties 
+    and not keys. If you search for keys using the Leaf parameter it will return a negative result.    
+  .NOTES
+    General notes
+      Created by: Brent Denny
+      Created on: 10-Jan-2020
+  #>
+  [cmdletbinding()]
+  Param (
+    [Parameter(Mandatory=$true)]
+    [string]$Path,
+    [switch]$Leaf
+  )
+  if ($Leaf -eq $true) {
+    Try {$KeyInfo = Get-ItemProperty -Path (Split-Path -Path $Path) -Name (Split-Path -Leaf -Path $Path) -ErrorAction Stop}
+    Catch {}
+    if ($KeyInfo) {$PathTestResult = $true}
+    else {$PathTestResult = $false}
+  }
+  else {
+    $PathTestResult = Test-Path $Path
+  }
+  $Hash = [ordered]@{
+    Path   = $Path
+    Exists = $PathTestResult
+    Leaf   = $Leaf
+  }
+  $Obj = New-Object -TypeName psobject -Property $Hash
+  return $Obj
+}
