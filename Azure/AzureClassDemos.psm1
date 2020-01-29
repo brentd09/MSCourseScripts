355d887f277bf9db5d05cd108ccabc9263ad13c6
Brent Denny <brent.denny@ddls.com.au>
2020-01-30 09:56:13 +1000

    Cleaned up the variable names

--- Azure/AzureClassDemos.psm1
+++ Azure/AzureClassDemos.psm1
@@ -6,6 +6,7 @@
   $VMs = @( 
     @{
       Name = 'demovm1'
+      NICName = 'NIC1'
       VNet = 'vnet1'
       Subnet = 'subnet1'
       SecurityGroup = 'secgrp1'
@@ -15,6 +16,7 @@
     } , 
     @{
       Name = 'demovm2'
+      NICName = 'NIC2'
       VNet = 'vnet2'
       Subnet = 'subnet2'
       SecurityGroup = 'secgrp2'
@@ -27,15 +29,15 @@
   New-AzResourceGroup -Name $ResourceGroup -Location $Location
   foreach ($VM in $VMs) {
     $SubnetConfig = New-AzVirtualNetworkSubnetConfig -Name $VM.Subnet -AddressPrefix $VM.SubnetPrefix
-    $vnet = New-AzVirtualNetwork -ResourceGroupName $ResourceGroup -Location $Location -Name $VM.VNet -AddressPrefix $VM.VNetPrefix -Subnet $subnetConfig
-    $pip = New-AzPublicIpAddress -ResourceGroupName $ResourceGroup -Location $Location -Name "mypublicdns$(Get-Random)" -AllocationMethod Static -IdleTimeoutInMinutes 4
-    $nsgRuleRDP = New-AzNetworkSecurityRuleConfig -Name myNetworkSecurityGroupRuleRDP  -Protocol Tcp -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389 -Access Allow
-    $nsg = New-AzNetworkSecurityGroup -ResourceGroupName $ResourceGroup -Location $Location -Name myNetworkSecurityGroup -SecurityRules $nsgRuleRDP
-    $nic = New-AzNetworkInterface -Name myNic -ResourceGroupName $ResourceGroup -Location $Location -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
-    $vmConfig = New-AzVMConfig -VMName $vmName -VMSize Standard_D1 | 
+    $VNet = New-AzVirtualNetwork -ResourceGroupName $ResourceGroup -Location $Location -Name $VM.VNet -AddressPrefix $VM.VNetPrefix -Subnet $subnetConfig
+    $PubIp = New-AzPublicIpAddress -ResourceGroupName $ResourceGroup -Location $Location -Name "mypublicdns$(Get-Random)" -AllocationMethod Static -IdleTimeoutInMinutes 4
+    $NSGRuleRDP = New-AzNetworkSecurityRuleConfig -Name myNetworkSecurityGroupRuleRDP  -Protocol Tcp -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389 -Access Allow
+    $NSG = New-AzNetworkSecurityGroup -ResourceGroupName $ResourceGroup -Location $Location -Name myNetworkSecurityGroup -SecurityRules $NSGRuleRDP
+    $NIC = New-AzNetworkInterface -Name myNic -ResourceGroupName $ResourceGroup -Location $Location -SubnetId $VNet.Subnets[0].Id -PublicIpAddressId $PubIp.Id -NetworkSecurityGroupId $NSG.Id
+    $VMConfig = New-AzVMConfig -VMName $vmName -VMSize Standard_D1 | 
       Set-AzVMOperatingSystem -Windows -ComputerName $VM.Name -Credential $cred | 
       Set-AzVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus '2016-Datacenter' -Version latest | 
-      Add-AzVMNetworkInterface -Id $nic.Id
-    New-AzVM -ResourceGroupName $ResourceGroup -Location $Location -VM $vmConfig
+      Add-AzVMNetworkInterface -Id $NIC.Id
+    New-AzVM -ResourceGroupName $ResourceGroup -Location $Location -VM $VMConfig
   }  
 }   
