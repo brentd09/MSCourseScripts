97d24ab821bdd3e88f8bc4c83727310b2d44204d
Brent Denny <brent.denny@ddls.com.au>
2020-01-30 06:29:04 +1000

    almost got the VMs sorted

--- Azure/AzureClassDemos.psm1
+++ Azure/AzureClassDemos.psm1
@@ -1,11 +1,11 @@
 ﻿function Initialize-DemoResGrp {
   $Loc = 'EastUS'
-  $ResGrpName = demoresgroup  
+  $ResGrpName = 'demoresgroup'
   New-AzResourceGroup -Name $ResGrpName -Location $Loc 
 }
 function Initialize-DemoVNetPair {
   $Loc = 'EastUS'
-  $ResGrpName = demoresgroup
+  $ResGrpName = 'demoresgroup'
   $Net1 = [ordered]@{
     VNetName   = 'vnet1'
     VnetCIDR   = '10.2.0.0/16'
@@ -30,5 +30,31 @@ function Initialize-DemoVNetPair {
 }
 
 function Initialize-DemoVMPair {
-  
+  $Loc = 'EastUS'
+  $ResGrpName = 'demoresgroup'
+  $VM1 = @{
+    Name = 'demovm1'
+    VNet = 'vnet1'
+    Subnet = 'subnet1'
+    SecurityGroup = 'secgrp1'
+    PublicIP = 'publicip1'
+  }  
+  $VM2 = @{
+    Name = 'demovm2'
+    VNet = 'vnet2'
+    Subnet = 'subnet2'
+    SecurityGroup = 'secgrp2'
+    PublicIP = 'publicip2'
+  }
+
+  $cred = Get-Credential
+  $vmInfo1 = New-AzVMConfig -VMName $VM1.Name -VMSize Standard_D1
+  $vmInfo1 = Set-AzVMOperatingSystem -VM $VMInfo1 -Windows -ComputerName $VM1.Name -Credential $cred -ProvisionVMAgent -EnableAutoUpdate 
+  $vmInfo1 = Set-AzVMSourceImage -VM $VMInfo1 -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus '2016-Datacenter' -Version latest
+  New-AzVm -ResourceGroupName $ResGrpName -Name $VM1.Name -Location $Loc -VirtualNetworkName $VM1.VNet -SubnetName $VM1.Subnet -SecurityGroupName $VM1.SecurityGroup -PublicIpAddressName $VM1.PublicIP -OpenPorts 80,3389
+
+  $vmInfo2 = New-AzVMConfig -VMName $VM2.Name -VMSize Standard_D1
+  $vmInfo2 = Set-AzVMOperatingSystem -VM $VMInfo2 -Windows -ComputerName $VM2.Name -Credential $cred -ProvisionVMAgent -EnableAutoUpdate 
+  $vmInfo2 = Set-AzVMSourceImage -VM $VMInfo2 -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus '2016-Datacenter' -Version latest
+  New-AzVm -ResourceGroupName $ResGrpName -Name $VM2.Name -Location $Loc -VirtualNetworkName $VM2.VNet -SubnetName $VM2.Subnet -SecurityGroupName $VM2.SecurityGroup -PublicIpAddressName $VM2.PublicIP -OpenPorts 80,3389
 }
