9ad5f340ed92e4e7728b70e0178444d1dccd61d5
Brent Denny <brent.denny@ddls.com.au>
2020-01-29 23:04:01 +1000

    new az class demos module

---
+++ Azure/AzureClassDemos.psm1
@@ -0,0 +1,27 @@
+﻿function Initialize-VNetPair {
+  $Loc = 'EastUS'
+  $ResGrpName = demoresgroup
+  $Net1 = [ordered]@{
+    VNetName   = 'vnet1'
+    VnetCIDR   = '10.2.0.0/16'
+    SubnetName = 'subnet1'
+    SubnetCIDR = '10.2.0.0/24'
+  }
+  
+  $Net2 = [ordered]@{
+    VNetName   = 'vnet2'
+    VnetCIDR   = '12.3.0.0/16'
+    SubnetName = 'subnet2'
+    SubnetCIDR = '12.3.0.0/24'
+  }
+  
+  New-AzResourceGroup -Name $ResGrpName -Location $Loc 
+  $Subnet1 = New-AzVirtualNetworkSubnetConfig -Name $Net1.SubnetName -AddressPrefix $Net1.SubnetCIDR 
+  $Subnet2 = New-AzVirtualNetworkSubnetConfig -Name $Net2.SubnetName -AddressPrefix $Net2.SubnetCIDR 
+  New-AzVirtualNetwork -Name $Net1.VNetName -ResourceGroupName $ResGrpName -Location $Loc -AddressPrefix $Net1.VnetCIDR -Subnet $Subnet1
+  New-AzVirtualNetwork -Name $Net2.VNetName -ResourceGroupName $ResGrpName -Location $Loc -AddressPrefix $Net2.VnetCIDR -Subnet $Subnet2
+}
+
+function Initialize-VMPair {
+  
+}
