a8417d395dc0211f6171229f0e0f71b2ae155b6b
Brent Denny <brent.denny@ddls.com.au>
2020-02-11 02:25:38 +1030

    error checking in place

--- Office/OffceTools.psm1
+++ Office/OffceTools.psm1
@@ -14,6 +14,12 @@ function Export-ExcelToPDF {
     Export-ExcelToPDF -ExcelFilePath 'c:\file.xlsx' -MacroName 'Macro1'
     This will save the spreadsheet to a pdf in the path c:\file.pdf after 
     executing Macro1
+  .EXAMPLE
+    Export-ExcelToPDF -ExcelFilePath 'c:\file.xlsx' -MacroName 'Macro1' -OverWritePDF
+    This will save the spreadsheet to a pdf in the path c:\file.pdf after 
+    executing Macro1 if the PDF file already exists it will be overwitten.
+    if the -OverWritePDF is not added and it does exist then the current PDF
+    file will be renamed with a datestamp in the filename.
   .EXAMPLE 
     Export-ExcelToPDF -ExcelFilePath 'c:\file.xlsx' -MacroName 'Macro1'
     This will save the spreadsheet to a pdf in the path c:\file.pdf after
@@ -30,21 +36,48 @@ function Export-ExcelToPDF {
   Param(
     [Parameter(Mandatory=$true)]
     [string]$ExcelFilePath,
-    [string]$MacroName = 'MacroName'
+    [string]$MacroName = 'MacroName',
+    [switch]$OverwritePDF
   )
   if (Test-Path $ExcelFilePath) {
-    $PdfFilePath = $ExcelFilePath -replace '^(.*)\.x[a-z]{2,3}$','$1.pdf'
-    If ([string]::IsNullOrEmpty($PdfFilePath)) {break}
-    $ExcelFixedFormat = 'Microsoft.Office.Interop.Excel.xlFixedFormatTypeù' -as [type]
-    $ExcelComObj = New-Object -ComObject excel.application
-    $ExcelComObj.visible = $false
-    $ExcelWorkBook = $ExcelComObj.workbooks.open($ExcelFilePath, 3)
+    $PdfFilePath = ($ExcelFilePath -replace '^(.*)\.x[a-z]{2,3}$','$1.pdf').ToLower()
+    If ([string]::IsNullOrEmpty($PdfFilePath)) {break}    
+    if (Test-Path $PdfFilePath) {
+      if ($OverwritePDF -eq $false) {
+        $DateStamp = Get-Date -UFormat '%Y%m%d%H%M'
+        $BackupName = $PdfFilePath.TrimEnd('.pdf') + $DateStamp + '.pdf'
+        Rename-Item -Path $PdfFilePath -NewName $BackupName
+      }
+      else {Remove-Item -Force -Path $PdfFilePath}
+    }
+    $ErrorActionPreference = 'Stop'
+    try {
+      $ExcelFixedFormat = 'Microsoft.Office.Interop.Excel.xlFixedFormatTypeù' -as [type]
+      $ExcelComObj = New-Object -ComObject excel.application
+      $ExcelComObj.visible = $false
+      $ExcelWorkBook = $ExcelComObj.workbooks.open($ExcelFilePath, 3)
+    }
+    catch {
+      Write-Warning "There was a problem reading $ExcelFilePath"
+      $ExcelComObj.Workbooks.close()
+      $ExcelComObj.Quit()
+      break
+    }
     $ExcelApp = $ExcelComObj.Application
     $ExcelApp.Run($MacroName)
-    $ExcelWorkBook.Saved = $true
-    "Saving PDF file $PdfFilePath"
-    $ExcelWorkBook.ExportAsFixedFormat($ExcelFixedFormat::xlTypePDF, $PdfFilePath)
-    $ExcelComObj.Workbooks.close()
-    $ExcelComObj.Quit()
+    try {
+      $ExcelWorkBook.Saved = $true
+      Write-Verbose "Saving PDF file $PdfFilePath"
+      $ExcelWorkBook.ExportAsFixedFormat($ExcelFixedFormat::xlTypePDF, $PdfFilePath)
+    } 
+    Catch {
+      Write-Warning "There was an error writing the files"
+      break
+    }
+    finally {
+      $ExcelComObj.Workbooks.close()
+      $ExcelComObj.Quit()
+    }
   }
+  else {Write-Warning "$ExcelFilePath does not exist"}
 }
