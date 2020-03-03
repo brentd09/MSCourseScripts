ffd9fc09978788f2c02510e19e08064ee6f4de99
Brent Denny <brent.denny@ddls.com.au>
2020-03-03 11:23:10 +1000

    pipe and multiple imput to the wol script

--- Networking/WakeOnLanTools.psm1
+++ Networking/WakeOnLanTools.psm1
@@ -17,22 +17,32 @@ function Send-WakeOnLan {
       The IPAddress address where the WOL packet will be sent to
     .EXAMPLE 
       Send-WakeOnLan -MACAddress 00:11:32:21:2D:11 
+    .EXAMPLE 
+      Send-WakeOnLan -MACAddress 00:11:32:21:2D:11,00:11:32:21:2E:15       
   #>
   
   [CmdletBinding()]
   param(
-    [Parameter(Mandatory=$True,Position=1)]
-    [string]$MACAddress,
+    [Parameter(Mandatory=$True,Position=1,
+               ValueFromPipeline,
+               ValueFromPipelineByPropertyName)]
+    [string[]]$MACAddress,
     [string]$IPAddress = "255.255.255.255", 
     [int]$PortNumber = 9
   )
-  $BroadcastAddress = [Net.IPAddress]::Parse($IPAddress)
-  $MACAddress = $MACAddress -replace '[:.-]',''
-  $Target = 0,2,4,6,8,10 | ForEach-Object {
-    [convert]::ToByte($MACAddress.substring($_,2),16)
+  begin {}
+  process {
+    foreach ($MAC in $MACAddress) {
+      $BroadcastAddress = [Net.IPAddress]::Parse($IPAddress)
+      $MAC = $MAC -replace '[:.-]',''
+      $Target = 0,2,4,6,8,10 | ForEach-Object {
+        [convert]::ToByte($MAC.substring($_,2),16)
+      }
+      $Packet = (,[byte]255 * 6) + ($Target * 16)
+      $UDPclient = new-Object System.Net.Sockets.UdpClient
+      $UDPclient.Connect($BroadcastAddress,$PortNumber)
+      [void]$UDPclient.Send($Packet, 102)
+    } 
   }
-  $Packet = (,[byte]255 * 6) + ($Target * 16)
-  $UDPclient = new-Object System.Net.Sockets.UdpClient
-  $UDPclient.Connect($BroadcastAddress,$PortNumber)
-  [void]$UDPclient.Send($Packet, 102) 
-}  
+  end {}
+}
