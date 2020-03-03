ef3756ed5f45f189b23f9f13ad0060ec163013da
Brent Denny <brent.denny@ddls.com.au>
2020-03-03 11:11:22 +1000

    New WOL script

---
+++ Networking/WakeOnLanTools.psm1
@@ -0,0 +1,38 @@
+function Send-WakeOnLan {
+  <# 
+    .SYNOPSIS  
+      This command sends a broadcast WakeOnLan packet
+    .DESCRIPTION
+      This command will send a WakeOnLan packet to a MAC and IPAddress however
+      the IPAddress is optional. The computer that you are sending the WOL packet 
+      to must be configured in the BIOS not to go into deep sleep, so that when 
+      it is shut down the NIC is still active.
+      For DELL BIOS make sure these settings are in place:
+      Enable USB Wake Support - Enabled
+      Deep Sleep - Disabled  
+      Wake On LAN - Enabled OR Enabled with PXE Boot
+    .PARAMETER MACAddress
+      The MAC address of the device that need to wake up
+    .PARAMETER IPAddress
+      The IPAddress address where the WOL packet will be sent to
+    .EXAMPLE 
+      Send-WakeOnLan -MACAddress 00:11:32:21:2D:11 
+  #>
+  
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory=$True,Position=1)]
+    [string]$MACAddress,
+    [string]$IPAddress = "255.255.255.255", 
+    [int]$PortNumber = 9
+  )
+  $BroadcastAddress = [Net.IPAddress]::Parse($IPAddress)
+  $MACAddress = $MACAddress -replace '[:.-]',''
+  $Target = 0,2,4,6,8,10 | ForEach-Object {
+    [convert]::ToByte($MACAddress.substring($_,2),16)
+  }
+  $Packet = (,[byte]255 * 6) + ($Target * 16)
+  $UDPclient = new-Object System.Net.Sockets.UdpClient
+  $UDPclient.Connect($BroadcastAddress,$PortNumber)
+  [void]$UDPclient.Send($Packet, 102) 
+}  
