9ead13e1538e558b75730f1bf70500dc35179937
Brent Denny <brent.denny@ddls.com.au>
2020-05-08 03:35:08 +1000

    updated help block

--- Azure/AzureVnetTools.psm1
+++ Azure/AzureVnetTools.psm1
@@ -3,21 +3,22 @@
   .SYNOPSIS
     Lists all of Virtual Networks in Azure and determines their type
   .DESCRIPTION
-    This cmdlet finds all of the Vnets that have peerings and determines 
-    if the peering is a global or regional type of peering. There are 
-    restrictions on what you can do with a global peering and so it is 
-    important to know which peering is what type  
+    This cmdlet finds all of the virtual networks that have peerings and 
+    determines if the peering is a global or regional type of peering. 
+    It will also show Virtual Networks that do not have any peerings as 
+    well. There are restrictions on what you can do with a global peering 
+    so it is important to know which peering is what type.
+    This also expects that you have already signed into Azure using 
+    Connect-AzAccount.
   .EXAMPLE
     Get-AzPeeringType
-    This expects that you have already signed into Azure using 
-    Connect-AzAccount it will then find all of the VNets that have 
-    peerings and determine each type
+    This will show all peerings (Global, Regional and NoPeering).
   .EXAMPLE
     Get-AzPeeringType -PeeringFilter Global
-    This will show peerings of a Global type   
+    This will only show peerings of a Global type   
   .EXAMPLE
     Get-AzPeeringType -PeeringFilter NoPeering
-    This will show VNets that do not have any peering configured       
+    This will only show VNets that do not have any peering configured       
   .PARAMETER PeeringFilter
     This will filter the peerings so that either a single peering type
     is shown or all are shown. The values for the PeeringFilter are:
@@ -29,6 +30,7 @@
     General notes
       Created by: Brent Denny
       Created on: 6 May 2020
+      Last Modified: 8 May 2020
   #>
   [cmdletbinding()]
   Param(
