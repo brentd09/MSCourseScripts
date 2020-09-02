0cabfe723f213d34a470055cb218bd7b54d5126f
brent denny <brent.denny@ddls.com.au>
2020-09-03 00:20:53 +1000

    changed the hidden key code

--- Azure/Copy-AzBlob.ps1
+++ Azure/Copy-AzBlob.ps1
@@ -1,38 +1,38 @@
 ﻿<#
 .Synopsis
-   Copy files to Azure Storage
+  Copy files to Azure Storage
 .DESCRIPTION
-   This script will copy all files from a given directory 
-   along with the directory structure up to a blob store 
-   in Azure
+  This script will copy all files from a given directory 
+  along with the directory structure up to a blob store 
+  in Azure
 .EXAMPLE
-   Copy-AzBlob -ContainerName Container1 -DirectoryPath c:\FilesToCopyToAzure -StorageAccountName brentstorageaccount1234
-   The script will prompy you for your Storage Access Key an then will
-   proceed to copy all of the files from your directiry into the blob
-   store.
+  Copy-AzBlob -ContainerName Container1 -DirectoryPath c:\FilesToCopyToAzure -StorageAccountName brentstorageaccount1234
+  The script will prompy you for your Storage Access Key an then will
+  proceed to copy all of the files from your directiry into the blob
+  store.
 .PARAMETER ContainerName
-   This is the name of the container within the storage account
+  This is the name of the container within the storage account
 .PARAMETER DirectoryPath
-   This is the directory from where the files will be copied, the 
-   entire directory structure is copied into the container
+  This is the directory from where the files will be copied, the 
+  entire directory structure is copied into the container
 .PARAMETER StorageAccountName
-   This is the unique name for the storage account 
+  This is the unique name for the storage account 
 .PARAMETER AccessKey
-   The script will automatically prompt you for this key
-   when you type the key into the console it will not show
-   and be replaced with stars
+  The script will automatically prompt you for this key
+  when you type the key into the console it will not show
+  and be replaced with stars
 .NOTES
-   General notes
-     Created by: Brent Denny
-     Created on: 02 Sep 2020 
+  General notes
+    Created by: Brent Denny
+    Created on: 02 Sep 2020 
 #>
 [CmdletBinding()]
 Param (
   [Parameter(DontShow)] 
   [string]$AccessKey = (
-    (New-Object System.Management.Automation.PSCredential (
-      'name',(Read-Host -Prompt "Enter Access Key" -AsSecureString ))
-    ).GetNetworkCredential().password
+    [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+      [Runtime.InteropServices.Marshal]::SecureStringToBSTR((Read-Host -Prompt "Enter Access Key" -AsSecureString ))
+    )     
   ),
   [Parameter(Mandatory=$true)] 
   [string]$ContainerName,
