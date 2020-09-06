3d8483ebc242680d90db654bd0a8b231402e5a7e
brent denny <brent.denny@ddls.com.au>
2020-09-06 21:49:52 +1000

    activity log script

---
+++ Azure/Get-AzActivityLogSimplified.ps1
@@ -0,0 +1,11 @@
+﻿ [CmdletBinding()]
+ Param(
+   [int]$MaxRecords = 20
+ )
+ 
+ Get-AzActivityLog -MaxRecord $MaxRecords -WarningAction SilentlyContinue | 
+  Select-Object -Property @{n='ResourceID';e={($_.ResourceId )}}, 
+                          @{n='OperationName';e={($_.OperationName.Value )}}, 
+                          @{n='Status';e={$_.Status.Value}},
+                          SubmissionTimestamp,
+                          CorrelationId
