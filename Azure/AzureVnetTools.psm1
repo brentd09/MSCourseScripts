ee1f0a28fe1ddd94d6cba9c99688b615b88db4d8
brent denny <brent.denny@ddls.com.au>
2020-10-23 23:43:18 +1000

    GUI vnet tool

--- Azure/AzureVnetTools.psm1
+++ Azure/AzureVnetTools.psm1
@@ -84,3 +84,134 @@
   }  
   catch { Write-Warning 'An error occured trying to access the Virtual Networks'}
 }
+
+function Find-AzureSubnetting {
+  Add-Type -AssemblyName System.Windows.Forms
+  [System.Windows.Forms.Application]::EnableVisualStyles()
+  
+  $Form                            = New-Object system.Windows.Forms.Form
+  $Form.ClientSize                 = New-Object System.Drawing.Point(521,625)
+  $Form.text                       = "Azure VNet Address Calculator"
+  $Form.TopMost                    = $false
+  
+  $VNetOctet1                      = New-Object system.Windows.Forms.TextBox
+  $VNetOctet1.multiline            = $false
+  $VNetOctet1.width                = 53
+  $VNetOctet1.height               = 40
+  $VNetOctet1.location             = New-Object System.Drawing.Point(50,70)
+  $VNetOctet1.Font                 = New-Object System.Drawing.Font('Microsoft Sans Serif',13)
+  
+  $VNetOctet2                      = New-Object system.Windows.Forms.TextBox
+  $VNetOctet2.multiline            = $false
+  $VNetOctet2.width                = 53
+  $VNetOctet2.height               = 40
+  $VNetOctet2.location             = New-Object System.Drawing.Point(120,70)
+  $VNetOctet2.Font                 = New-Object System.Drawing.Font('Microsoft Sans Serif',13)
+  
+  $VNetOctet3                      = New-Object system.Windows.Forms.TextBox
+  $VNetOctet3.multiline            = $false
+  $VNetOctet3.width                = 53
+  $VNetOctet3.height               = 40
+  $VNetOctet3.location             = New-Object System.Drawing.Point(190,70)
+  $VNetOctet3.Font                 = New-Object System.Drawing.Font('Microsoft Sans Serif',13)
+  
+  $VNetOctet4                      = New-Object system.Windows.Forms.TextBox
+  $VNetOctet4.multiline            = $false
+  $VNetOctet4.width                = 53
+  $VNetOctet4.height               = 40
+  $VNetOctet4.location             = New-Object System.Drawing.Point(260,70)
+  $VNetOctet4.Font                 = New-Object System.Drawing.Font('Microsoft Sans Serif',13)
+  
+  $VNetCIDR                        = New-Object system.Windows.Forms.TextBox
+  $VNetCIDR.multiline              = $false
+  $VNetCIDR.width                  = 53
+  $VNetCIDR.height                 = 40
+  $VNetCIDR.location               = New-Object System.Drawing.Point(350,70)
+  $VNetCIDR.Font                   = New-Object System.Drawing.Font('Microsoft Sans Serif',13)
+  
+  $Label1                          = New-Object system.Windows.Forms.Label
+  $Label1.text                     = "."
+  $Label1.AutoSize                 = $true
+  $Label1.width                    = 25
+  $Label1.height                   = 10
+  $Label1.location                 = New-Object System.Drawing.Point(109,80)
+  $Label1.Font                     = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+  
+  $Label2                          = New-Object system.Windows.Forms.Label
+  $Label2.text                     = "."
+  $Label2.AutoSize                 = $true
+  $Label2.width                    = 25
+  $Label2.height                   = 10
+  $Label2.location                 = New-Object System.Drawing.Point(180,80)
+  $Label2.Font                     = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+  
+  $Label3                          = New-Object system.Windows.Forms.Label
+  $Label3.text                     = "."
+  $Label3.AutoSize                 = $true
+  $Label3.width                    = 25
+  $Label3.height                   = 10
+  $Label3.location                 = New-Object System.Drawing.Point(250,80)
+  $Label3.Font                     = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+  
+  $Label5                          = New-Object system.Windows.Forms.Label
+  $Label5.text                     = "/"
+  $Label5.AutoSize                 = $true
+  $Label5.width                    = 25
+  $Label5.height                   = 10
+  $Label5.location                 = New-Object System.Drawing.Point(330,77)
+  $Label5.Font                     = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+  
+  $Label4                          = New-Object system.Windows.Forms.Label
+  $Label4.text                     = "Azure VNet Address Space"
+  $Label4.AutoSize                 = $true
+  $Label4.width                    = 29
+  $Label4.height                   = 22
+  $Label4.location                 = New-Object System.Drawing.Point(63,40)
+  $Label4.Font                     = New-Object System.Drawing.Font('Microsoft Sans Serif',13)
+  
+  $Label6                          = New-Object system.Windows.Forms.Label
+  $Label6.text                     = "CIDR Mask for Subnet"
+  $Label6.AutoSize                 = $true
+  $Label6.width                    = 25
+  $Label6.height                   = 20
+  $Label6.location                 = New-Object System.Drawing.Point(65,129)
+  $Label6.Font                     = New-Object System.Drawing.Font('Microsoft Sans Serif',13)
+  
+  $CIDRMask                        = New-Object system.Windows.Forms.TextBox
+  $CIDRMask.multiline              = $false
+  $CIDRMask.width                  = 54
+  $CIDRMask.height                 = 20
+  $CIDRMask.location               = New-Object System.Drawing.Point(258,123)
+  $CIDRMask.Font                   = New-Object System.Drawing.Font('Microsoft Sans Serif',13)
+  
+  $SubnetList                      = New-Object system.Windows.Forms.ListBox
+  $SubnetList.text                 = "listBox"
+  $SubnetList.width                = 414
+  $SubnetList.height               = 400
+  $SubnetList.location             = New-Object System.Drawing.Point(48,177)
+  $SubnetList.Font                 = New-Object System.Drawing.Font('Microsoft Sans Serif',13)
+  
+  $Calc                            = New-Object system.Windows.Forms.Button
+  $Calc.text                       = "Calculate"
+  $Calc.width                      = 90
+  $Calc.height                     = 30
+  $Calc.location                   = New-Object System.Drawing.Point(393,119)
+  $Calc.Font                       = New-Object System.Drawing.Font('Microsoft Sans Serif',13)
+  $Calc.ForeColor                  = [System.Drawing.ColorTranslator]::FromHtml("#000000")
+  $Calc.BackColor                  = [System.Drawing.ColorTranslator]::FromHtml("#b8e986")
+  
+  $Form.controls.AddRange(@($VNetOctet1,$VNetOctet2,$VNetOctet3,$VNetOctet4,$VNetCIDR,$Label1,$Label2,$Label3,$Label5,$Label4,$Label6,$CIDRMask,$SubnetList,$Calc))
+  
+  $Calc.Add_Click({ Calcfn })
+  
+  function Calcfn { 
+    # test if IP is valid 
+    # test if IP is valid with VNet snm
+    # Create subnets based on subnet mask
+    # Add subnets to list
+    [ipaddress]$VnetIP = $VNetOctet1.Text + '.' + $VNetOctet2.Text + '.' + $VNetOctet3.Text + '.' + $VNetOctet4.Text 
+    $SubnetList.Items.Add($VnetIP.IPAddressToString)
+  } 
+  
+  [void]$Form.ShowDialog()
+}
