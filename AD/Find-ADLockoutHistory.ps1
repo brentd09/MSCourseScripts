419233cb2fe9cf0e7b5d3ec26902f0500bc37582
Brent Denny <brent.denny@ddls.com.au>
2021-02-17 16:37:59 +1000

    Add speaker control class

---
+++ AD/Find-ADLockoutHistory.ps1
@@ -0,0 +1,16 @@
+﻿[Cmdletbinding()]
+Param (
+  [Parameter(Mandatory=$true)]
+  [string]$user
+)
+Clear-Host
+$PDC = (Get-ADDomainController -Discover -Service PrimaryDC).Name
+$DCs = (Get-ADDomainController -Filter *).Name
+foreach ($DC in $DCs) {
+  Write-Host -ForegroundColor Green "Checking Account lockout events on $DC for $user"
+  if ($DC -eq $PDC) {
+    Write-Host -ForegroundColor Green "$DC is the PDC"
+  }
+  Get-EventLog -LogName Security -ComputerName $DC | 
+   Where-Object {$_.EventID -in @(4740,4767,4770,4771) -and $_.Message -like "*$user*" }
+}
