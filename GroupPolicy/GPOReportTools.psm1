56cc65d3e476d80b84ba91b6d6348ba0f7c67588
brent denny <brent.denny@ddls.com.au>
2021-04-23 19:08:44 +1000

    selection of GPOS and help added

--- GroupPolicy/GPOReportTools.psm1
+++ GroupPolicy/GPOReportTools.psm1
@@ -14,6 +14,22 @@ function Invoke-GPOSettingReport {
     Invoke-GPOSettingReport -ReportDirectory 'e:\reports' -DomainName 'adatum.com' -ReportType 'html'
     This will get the GPOs from the adatum.com domain and create a file per GPO 
     in the E:\Reports folder.
+  .EXAMPLE
+    Invoke-GPOSettingReport -ReportDirectory 'e:\reports' -DomainName 'adatum.com' -ReportType 'html' -SelectGPOs
+    This will get the GPOs from the adatum.com domain and present a gui tool to select which 
+    GPOs to create reports for, this will then create a file per GPO in the E:\Reports folder.    
+  .PARAMETER ReportDirectory
+    This is the directory that the reports will be created in, the 
+    default value for this parameter is the users TMP directory found
+    by using [System.IO.Path]::GetTempPath() path
+  .PARAMETER DomainName
+    This is the DNS FQDN of the Active Directory domain name that the 
+    GPOs will be reported on.
+  .PARAMETER ReportType
+    There is only two choices; either HTML or XML
+  .PARAMETER SelectGPOs
+    This parameter flags this command to prompt via a Out-GridView GUI
+    to choose which of the GPOs will have reports created
   .NOTES
     General notes
       Created by : Brent Denny
@@ -25,17 +41,21 @@ function Invoke-GPOSettingReport {
     [string]$ReportDirectory = ([System.IO.Path]::GetTempPath()),
     [string]$DomainName,
     [ValidateSet('html','xml')]
-    [string]$ReportType = 'html'
+    [string]$ReportType = 'html',
+    [switch]$SelectGPOs
   )
   try {$AllGPOs = Get-Gpo -Domain $DomainName -All -ErrorAction Stop}
-  catch {Write-Warning "The GPOs could not be obtained from the system";break} 
+  catch {Write-Warning "The GPOs could not be obtained from the system";break}  if ($SelectGPOs -eq $true) {
+  if ($SelectGPOs -eq $true) {$GPOs = $AllGPOs | Select-Object -Property DisplayName,GUID | Out-GridView -OutputMode Multiple}
+  else {$GPOs = $AllGPOs}
+  if ($GPOs.Count -eq 0) {Write-Warning "You must select at least one GPO";break}
   if (-not (Test-Path -Path $ReportDirectory -PathType Container)) {
     $Parent = $ReportDirectory | Split-Path -Parent 
     $Leaf = $ReportDirectory | Split-Path -leaf
     try {New-Item -Path $Parent -Name $Leaf -ItemType Directory -ErrorAction Stop}
     catch {Write-Warning -Message "$ReportDirectory cound not be created";break}
   }
-  foreach ($GPO in $AllGPOs) {
+  foreach ($GPO in $GPOs) {
     $ReportFilePath =  $ReportDirectory.TrimEnd('\') + '\' + $GPO.Displayname + ".$ReportType"
     Get-GpoReport -Guid $GPO.Id -ReportType $ReportType | Out-File -FilePath $ReportFilePath
   }
