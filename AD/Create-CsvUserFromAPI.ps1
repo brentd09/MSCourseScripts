eddf2c2ee90349e450f1af5476788467d9d9a1f6
brent denny <brent.denny@ddls.com.au>
2021-04-26 23:09:20 +1000

    removed hidden chars

--- AD/Create-CsvUserFromAPI.ps1
+++ AD/Create-CsvUserFromAPI.ps1
@@ -1,4 +1,22 @@
-﻿function New-RandomPassword {    [cmdletbinding()]  Param (    [int]$NumberCount = 2,    [int]$UpperCount = 2,    [int]$LowerCount = 5,    [int]$SpecialCount = 1  )  do {    $Num = 2..9 | get-random -Count $NumberCount    $Upr = 65..90 | ForEach-Object {[char]$_} | get-random -count $UpperCount    $Lwr = 97..122 | ForEach-Object {[char]$_} | get-random -count $LowerCount    $Spl = 33,35,36,37,38,63,64  | ForEach-Object {[char]$_} | get-random -count $SpecialCount    $RawPswd = $Num + $Upr + $Lwr + $Spl    $RandPswd = $RawPswd | Sort-Object {Get-Random}    $ComplexPswd = -join ($RandPswd)  } until ($RandPswd[0] -match '[a-z]')  return $ComplexPswd }
+﻿function New-RandomPassword {  
+  [cmdletbinding()]
+  Param (
+    [int]$NumberCount = 2,
+    [int]$UpperCount = 2,
+    [int]$LowerCount = 5,
+    [int]$SpecialCount = 1
+  )
+  do {
+    $Num = 2..9 | get-random -Count $NumberCount
+    $Upr = 65..90 | ForEach-Object {[char]$_} | get-random -count $UpperCount
+    $Lwr = 97..122 | ForEach-Object {[char]$_} | get-random -count $LowerCount
+    $Spl = 33,35,36,37,38,63,64  | ForEach-Object {[char]$_} | get-random -count $SpecialCount
+    $RawPswd = $Num + $Upr + $Lwr + $Spl
+    $RandPswd = $RawPswd | Sort-Object {Get-Random}
+    $ComplexPswd = -join ($RandPswd)
+  } until ($RandPswd[0] -match '[a-z]')
+  return $ComplexPswd 
+}
 
 
 try {Remove-Item -Force e:\userPsw.rpt -ErrorAction stop}
