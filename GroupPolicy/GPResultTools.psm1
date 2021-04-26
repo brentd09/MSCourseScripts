964486c429a029b461e90882b8299dd49895e3f9
brent denny <brent.denny@ddls.com.au>
2021-04-26 23:04:52 +1000

    fixed gporesults

--- GroupPolicy/GPResultTools.psm1
+++ GroupPolicy/GPResultTools.psm1
@@ -1,11 +1,11 @@
-﻿function Get-GPOProcess {
+﻿function Get-GPOApplied {
   <#
   .SYNOPSIS
     Shows all policies that applied to a set of computers
   .DESCRIPTION
-    Lists user and computer policies that apply to one or more computers
+    Lists user and computer policies by Name or GUID that apply to one or more computers
   .EXAMPLE
-    Get-GPOProcess -ComputerName LON-CL1,LON-CL2,LON-CL3 | Format-List
+    Get-GPOApplied -ComputerName LON-CL1,LON-CL2,LON-CL3 | Format-List
     This command gets the RSOP information from GPResult and stores it in 
     XML files in the $Env:TEMP directory and then extracts the policy info
     from these XML result files to produce an object that shows the GPOs 
@@ -15,8 +15,9 @@
     One or more computernames can be added to compare the policys applied for each  
   .NOTES
     General notes
-      Created By: Brent Denny
-      Created On: 26 Aug 2019
+      Created By : Brent Denny
+      Created On : 26 Aug 2019
+      Modified On: 26 Apr 2021
   #>
   [cmdletbinding()]
   Param (
@@ -30,19 +31,19 @@
         Write-Warning "The XML file $XMLFilePAth exists, skipping $Computer, delete file manually and retry"
         throw
       } #END if
-      GPResult -scope 'computer' -S $Computer -x $XMLFilePath
+      GPResult -S $Computer -x $XMLFilePath
       [xml]$GPOResultXML = get-content $XMLFilePath
       if (Test-Path $XMLFilePath) {Remove-Item $XMLFilePath -force}
-      $ComputerRSOP = $GPOResultXML.Rsop.computerresults.GPO.Name -join "`n"
-      $UserRSOP     = $GPOResultXML.Rsop.userresults.GPO.Name -join "`n"
+      $ComputerRSOP = $GPOResultXML.Rsop.computerresults.GPO.Name 
+      $UserRSOP     = $GPOResultXML.Rsop.userresults.GPO.Name 
       $ObjectProperties = [ordered]@{
         ComputerName       = $Computer
-        UserGPOProcess     = $UserRSOP
-        ComputerGPOProcess = $ComputerRSOP
+        UserGPOApplied     = $UserRSOP
+        ComputerGPOApplied = $ComputerRSOP
       } #END Hashtable
       New-Object -TypeName psobject -Property $ObjectProperties
     } #END try
-    catch {}
+    catch {Write-Warning "An error occured while retrieving the GPO results";throw}
     finally {$ErrorActionPreference='Continue'}
   } #END foreach
 } #END function
