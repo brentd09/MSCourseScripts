0aa1c47308146edbcc9e9c164d0e02a28ab6aef1
brent denny <brent.denny@ddls.com.au>
2021-10-02 23:38:12 +1000

    fixed help

--- AD/AdDomainConfig.psm1
+++ AD/AdDomainConfig.psm1
@@ -1,28 +1,30 @@
 function Get-ADConfigurationLevel {
   <#
   .SYNOPSIS
-    Determine SYSVOL replication type
+    Determine Domain and Forest functional levels and SYSVOL replication type
   .DESCRIPTION
-    Determines whether the SYSVOL replication is via FRS or DFS-R. The FRS replication
+    Determines what doamain and forest fuctional levels are ser and 
+    whether the SYSVOL replication is via FRS or DFS-R. The FRS replication
     is a very old technology and was in place from Windows 2000 up until the end of 
     Windows server 2003. Domain Controllers that were in-place upgraded will still be 
     using FRS unless the DFS migration was undertaken 
   .EXAMPLE
     Get-ADConfigurationLevel -ComputerName 'LON-DC1' -Domain 'adatum.com'
     This will check for the the domain adatum.com by communicating with the DC LON-DC1
-    to determine which type of SYSVOL replication is being used.
+    to determine functional levels and the type of SYSVOL replication is being used.
   .PARAMETER ComputerName
     This directs the command to communicate with this computer which will be a domain
-    controller withing the domain that is being checked.
+    controller within the domain that is being checked.
   .PARAMETER Domain
     This is the DNS name of the domain that is being checked
   .NOTES
     General notes
       Created By:    Brent Denny 
       Created On:    29 Sep 2021
-      Last Modified: 29 Sep 2021
+      Last Modified: 02 Oct 2021
       Change log:
         290921 - Created this script based on a question in class
+        021021 - added the domain fuctional levels as well
   #>
   [CmdletBinding()]
   Param(
