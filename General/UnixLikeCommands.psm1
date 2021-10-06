d383f6809bb913981db69b44a866e3992e8a78f7
brent denny <brent.denny@ddls.com.au>
2021-10-07 07:49:25 +1000

    unix like commands

---
+++ General/UnixLikeCommands.psm1
@@ -0,0 +1,38 @@
+﻿function Find-Executable {
+  <#
+  .Synopsis
+     Searches the $Path variable for a selected file
+  .DESCRIPTION
+     Searches through the $Path variable for matching files
+  .EXAMPLE
+     Find-Executable -Name Write
+     This will look for a file called 'Write' somewhere in the $Path directories
+  .EXAMPLE
+     Find-Executable -Name Write.exe
+     This will look for a file called 'Write.exe' somewhere in the $Path directories
+  .PARAMETER Name
+     This is the name of the executable file you can include the extension or not
+  .NOTES
+     Created By: Brent Denny
+     Created On: 5 Oct 2012
+  #>
+  [CmdletBinding()]
+  Param (
+    [string]$Name = ''
+  )
+  if ($Name -ne '' ) {
+    if ($Name -notmatch '\..{3,}$') {$Name = ($Name -split '\.')[0] +'.*'}
+    foreach ($PathElement in ($env:Path -split ';')) {
+      $FullPathToExe = $PathElement.trimend('\')+'\'+$Name
+      try {
+        if (Test-Path -Path $FullPathToExe -ErrorAction Stop) {
+          Get-ChildItem $FullPathToExe -File | Select-Object Mode,LastWriteTime,FullName
+        }
+      }
+      catch {}
+    }
+  }
+  else {
+    write-warning "You must search for an executable`nUsage: Where-Executable -Executable write"
+  }
+}
