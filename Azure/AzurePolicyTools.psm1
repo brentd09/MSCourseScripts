c2e61a036198e2f32748997de4abca12831ba1f3
brent denny <brent.denny@ddls.com.au>
2021-11-08 16:51:00 +1000

    whole new policy function

--- Azure/AzurePolicyTools.psm1
+++ Azure/AzurePolicyTools.psm1
@@ -6,36 +6,31 @@ function Invoke-AzPolicyEvaluation {
     This triggers the Azure Policy to trigger now rather than 
     waiting for the the 1 hr cycle built into Azure
   .EXAMPLE
-    Invoke-AzPolicyEvaluation -$SubscriptionId 12342113-1234-4491-80c6-1234f54182b5
+    Invoke-AzPolicyEvaluation
     This triggers the Policy evaluation within this subscription.
     You can locate the subscription ID by using the Get-AzSubscription PowerShell cmdlet
   .NOTES
     General notes
-      Created by: Brent Denny
-      Created on: 07 Oct 2020
+      Created by:   Brent Denny
+      Created on:   07 Oct 2020
+      Last Edited : 09 Nov 2021
       Original Source code from: https://www.miru.ch/how-to-manually-trigger-an-azure-policy-evaluation-cycle/
   #>
-  Param (
-    [Parameter(Mandatory=$true)]
-    [ValidatePattern('[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}')]
-    [string]$SubscriptionId 
-    
-  )
-  $uri = "https://management.azure.com/subscriptions/$SubscriptionId/providers/Microsoft.PolicyInsights/policyStates/latest/triggerEvaluation?api-version=2018-07-01-preview"
-  try {
-    $azContext = Get-AzContext -ErrorAction Stop
-    if (-not $azContext) {throw}
-  }
+  Param()
+  try {Get-AzSubscription -ErrorAction Stop}
+  catch {Connect-AzAccount}
+  try {$subscriptionId = (Get-AzSubscription -ErrorAction Stop).Id}
   catch {
-    try {
-      Connect-AzAccount -ErrorAction stop
-      $azContext = Get-AzContext -ErrorAction Stop
-    }
-    catch {
-      Write-Warning 'Access to Azure was not initiated correctly'
-      break
-    }
+    Write-Warning "Error accessing your Azure account"
+    break
   }
+  $PolicyInsights = Get-AzResourceProvider Microsoft.PolicyInsights  
+  while ($PolicyInsights.RegistrationState -contains $false) {
+    Write-Warning "Waiting for the Resource Provider to register"
+    Start-Sleep -Seconds 15
+  }
+  $uri = "https://management.azure.com/subscriptions/$subscriptionId/providers/Microsoft.PolicyInsights/policyStates/latest/triggerEvaluation?api-version=2018-07-01-preview"
+  $azContext = Get-AzContext
   $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
   $profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
   $token = $profileClient.AcquireAccessToken($azContext.Tenant.Id)
@@ -43,5 +38,5 @@ function Invoke-AzPolicyEvaluation {
       'Content-Type'='application/json'
       'Authorization'='Bearer ' + $token.AccessToken
   }
-  Invoke-RestMethod -Method Post -Uri $uri -UseBasicParsing -Headers $authHeader
+  Invoke-RestMethod -Method Post -Uri $uri -UseBasicParsing -Headers $authHeader -Debug
 }
