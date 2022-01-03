19f089382125fc20dc875ce7c4c1613b3dc38c50
Brent Denny <brent.denny@ddls.com.au>
2022-01-04 07:55:03 +1000

    IPconfigtools

---
+++ Networking/2.0.0/IPConfigTools.psm1
@@ -0,0 +1,36 @@
+﻿function Get-IPConfig {
+  [CmdletBinding()]
+  Param()
+  $NetAdapters = Get-NetAdapter -IncludeHidden | Where-Object {$_.MacAddress -notmatch '^$'}
+  $WMIAdapters = Get-CimInstance -ClassName Win32_NetworkAdapter | Where-Object {$_.InterfaceIndex -in $NetAdapters.ifIndex}
+  $WMIAdapterConfigs = Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration | Where-Object {$_.InterfaceIndex -in $NetAdapters.ifIndex} 
+  $IPInterFaces = Get-NetIPInterface |  Where-Object {$_.ifIndex -in $NetAdapters.ifIndex}
+  foreach ($NetAdapter in $NetAdapters) {
+    $WMIAdapter     = $WMIAdapters | Where-Object {$_.InterfaceIndex -eq $NetAdapter.ifIndex}
+    $WMIAdapterConf = $WMIAdapterConfigs | Where-Object {$_.InterfaceIndex -eq $NetAdapter.ifIndex}
+    $IPInterFace    = $IPInterFaces | Where-Object {$_.ifIndex -eq $NetAdapter.ifIndex}
+    $AdapProp = [ordered]@{
+      HostName              = $NetAdapter.SystemName
+      AdapterName           = $NetAdapter.Name
+      InterfaceID           = $NetAdapter.ifIndex
+      Description           = $NetAdapter.InterfaceDescription
+      PhysicalAddress       = $NetAdapter.MacAddress
+      DHCPEnabled           = $WMIAdapterConf.DHCPEnabled
+      LinkLocalAddress      = $WMIAdapterConf.IPAddress | Where-Object {$_ -match '^FE80'}
+      IPv4Address           = $WMIAdapterConf.IPAddress | Where-Object {$_ -notmatch ':'}
+      IPv4SubnetMask        = $WMIAdapterConf.IPSubnet | Where-Object {$_ -match '\.'}
+      IPv4ConnectionState   = ($IPInterFace | Where-Object {$_.AddressFamily -eq 'IPv4'}).ConnectionState
+      IPv6Address           = $WMIAdapterConf.IPAddress | Where-Object {$_ -match ':' -and $_ -notmatch '^FE80'}
+      IPv6Mask              = $WMIAdapterConf.IPSubnet | Where-Object {$_ -notmatch '\.'}
+      IPv6ConnectionState   = ($IPInterFace | Where-Object {$_.AddressFamily -eq 'IPv6'}).ConnectionState
+      DHCPLeaseObtained     = $WMIAdapterConf.DHCPLeaseObtained
+      DHCPLeaseExpires      = $WMIAdapterConf.DHCPLeaseExpires
+      DefaultGateway        = $WMIAdapterConf.DefaultIPGateway
+      DHCPServer            = $WMIAdapterConf.DHCPServer
+      DNSServer             = $WMIAdapterConf.DNSServerSearchOrder
+    }
+    New-Object -TypeName psobject -Property $AdapProp 
+  }
+}
+
+Get-IPConfig 
