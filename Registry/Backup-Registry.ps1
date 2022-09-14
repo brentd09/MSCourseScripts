c6ce7626748c746129589d34affb1ff12d1b22fd
Brent Denny <brent.denny@ddls.com.au>
2022-09-15 07:24:34 +1000

    reg backup mods

--- Registry/Backup-Registry.ps1
+++ Registry/Backup-Registry.ps1
@@ -1,4 +1,18 @@
 function Backup-RegistryPath {
+  <#
+  .SYNOPSIS
+    A short one-line action-based description, e.g. 'Tests if a function is valid'
+  .DESCRIPTION
+    A longer description of the function, its purpose, common use cases, etc.
+  .NOTES
+    Information or caveats about the function e.g. 'This function is not supported in Linux'
+  .LINK
+    Specify a URI to a help page, this will show when Get-Help -Online is used.
+  .EXAMPLE
+    Test-MyTestFunction -Verbose
+    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+  #>
+  
   [CmdletBinding()]
   Param ([string]$RegPath = 'HKCU:\Software\Brent')
   $RegKeys = Invoke-Command {
