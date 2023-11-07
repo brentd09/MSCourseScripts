716808c386d1a3df2cf230ba3b2c1f3104b12f35
BrentD09 <brent.denny@ddls.com.au>
2023-11-07 16:14:59 +1000

    Update AzNsgAsgTools.psm1

--- Azure/AzNsgAsgTools.psm1
+++ Azure/AzNsgAsgTools.psm1
@@ -3,9 +3,33 @@ function Get-AzAsgToVMMapping {
   Param ()
 
   try {Connect-AzAccount -ErrorAction Stop}
-  catch {Write-Warning "Please download the AZ module from PowerShell Gallery before running this again";break}
+  catch {Write-Warning "Please download the AZ module from PowerShell Gallery before running this again"; break}
 
   $AllASGs = Get-AzApplicationSecurityGroup
   $AllAzNics = Get-AzNetworkInterface
   $AllAzNics | Select-Object Name,@{n='VM';e={$_.VirtualMachine.ID}},@{n='ASG';e={$_.IpConfigurations.ApplicationSecurityGroups.ID}} 
+}
+
+function Get-AzAsgMembership {
+  [CmdletBinding()]
+  Param ()  
+  
+  try {Connect-AzAccount -ErrorAction Stop}
+  catch {Write-Warning "Please download the AZ module from PowerShell Gallery before running this again"; break}
+  $AzNics = Get-AzNetworkInterface
+  $VMs    = Get-AzVM
+  $ASGs   = Get-AzApplicationSecurityGroup 
+  foreach ($ASG in $ASGs) {
+    $MatchingNics = $AzNics | Where-Object {($_.IpConfigurations.ApplicationSecurityGroupsText | ConvertFrom-Json).ID -eq $ASG.Id}
+    foreach ($MatchingNic in $MatchingNics) {
+      $MatchingVM = $VMs | Where-Object {$_.NetworkProfile.NetworkInterfaces.Id -contains $MatchingNic.Id} 
+      if ($MatchingVM) {
+        [PSCustomObject]@{
+          ASG = $ASG.Name
+          VM  = $MatchingVM.Name
+          NIC = $MatchingNic.Name
+        }
+      } 
+    } 
+  }
 }
